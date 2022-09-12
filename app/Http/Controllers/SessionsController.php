00d7921f6246d1494e6080935931de4bfd15cd6d
vaxlrus <vaxl.rus@gmail.com>
2022-09-12 20:24:08 +0300

    #8 Удалил мусор

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -14,7 +14,7 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
-    public function store(UserDeletingService $userDeletingService, UserRestorationService $userRestorationService)
+    public function store(UserRestorationService $userRestorationService)
     {
         $attributes = request()->validate([
             'email' => 'required|email',
