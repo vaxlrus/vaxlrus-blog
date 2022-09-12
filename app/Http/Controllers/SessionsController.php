3927eb609c3b8f7237c800f851d39028fb19c37e
vaxlrus <vaxl.rus@gmail.com>
2022-09-12 21:38:54 +0300

    #8 Сокрытие информации при невозможности восстановить аккаунт

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -23,7 +23,7 @@ class SessionsController extends Controller
 
         if (! $userRestorationService->isAccountRestorable($attributes['email'])) {
             throw ValidationException::withMessages([
-                'email' => 'Аккаунт не возможно восстановить'
+                'email' => 'Your provided credentials could not be verified.'
             ]);
         }
 
